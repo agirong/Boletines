a2965765769f4d16fe558c4afb9577299e14f30d
agirong <62667714+agirong@users.noreply.github.com>
2021-05-26 20:14:16 -0600

    pruebas page
    modificaciones de estructura

--- javascript/main.js
+++ javascript/main.js
@@ -1,7 +1,41 @@
 
+/* 
+let celdas = document.querySelectorAll("td");
 
-let links = document.querySelectorAll("a");
-
-links.forEach(link => {
-    console.log(link)
+celdas.forEach(function(td){
+    td.addEventListener('click',function(){
+        console.log(this);
+    })
 });
+ */
+
+//Obtenr loe elementos de la clase .close
+const links =document.querySelectorAll('.close');
+
+//recorrerlos
+links.forEach(function(link){
+    link.addEventListener("click",function(evt){
+        evt.preventDefault();
+        let content = document.querySelector('.content');
+
+        //quitar las animaciones 
+        
+        content.classList.remove("animate__fadeInDown");
+        content.classList.remove("animate__animated");
+        //agrefar clases 
+        content.classList.add("animate__fadeOutUp");
+        content.classList.add("animate__animated");
+
+        setTimeout(function(){
+            location.href="/Boletines";
+        },600);
+
+        location.href="/";
+        return false;
+    });
+});
+
+
+
+
+
